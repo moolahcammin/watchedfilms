f227c367d888c17999eda5549073a5c86a3ca511
Artem <realpoo14@gmail.com>
2021-04-05 04:13:24 +0300

    start work

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let numberOfFilms;
+"use strict";
+
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "100");
+console.log("Я посмотрел столько фильмов:" + numberOfFilms);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+}
