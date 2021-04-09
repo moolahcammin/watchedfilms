c97024b3c44ff812992571e0cea12e4b3364fe21
Artem <realpoo14@gmail.com>
2021-04-10 01:29:32 +0300

    finished second practice lesson

--- script.js
+++ script.js
@@ -8,13 +8,77 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из просмотренных фильмов?", ""),
-      b = prompt("Насколько оцените его?"),
-      c = prompt("Один из просмотренных фильмов?", ""),
-      d = prompt("Насколько оцените его?");
+// const a = prompt("Один из просмотренных фильмов?", ""),
+//       b = prompt("Насколько оцените его?", ""),
+//       c = prompt("Один из просмотренных фильмов?", ""),
+//       d = prompt("Насколько оцените его?", "");
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// Автоматизация вопросов
+
+// let num = 2;
+
+//1 способ
+
+// while (num < 4) {
+//     const a = prompt("Один из просмотренных фильмов?", ""),
+//           b = prompt("Насколько оцените его?", "");
+//           num++;
+//          
+//           if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//                         personalMovieDB.movies[a] = b;
+//                         console.log("done");
+//                     } else {
+//                         console.log("error");
+//                         num--;
+//                     }
+// }
+
+//2 способ
+
+// do {
+//     const a = prompt("Один из просмотренных фильмов?", ""),
+//           b = prompt("Насколько оцените его?", "");
+//           num++;
+          
+//         if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//         } else {
+//         console.log("error");
+//         num--;
+//     }
+// }
+// while (num <4);
+
+//3 способ
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из просмотренных фильмов?", ""),
+          b = prompt("Насколько оцените его?", "");
+
+        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log("done");
+        } else {
+            console.log("error");
+            i--;
+        }
+}
+
+
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
